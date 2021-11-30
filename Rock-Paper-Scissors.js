df6ae66602ec32580d3bc407263e5232c861265d
Ahmed-Benj <ahmedblaugrana@gmail.com>
2021-11-30 17:52:31 +0100

    player against computer play function

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,7 +1,14 @@
 console.log("Hello World");
 const playArray = ["Rock", "Paper", "Scissors"];
 
-console.log(computerPlay(playArray));
+let computerSelection = computerPlay(playArray);
+console.log("Computer played : " + computerSelection);
+
+let playerSelection = prompt("Enter your play: Paper, Rock or Scissors");
+console.log("You played : " + playerSelection);
+
+
+console.log(playRound(playerSelection,computerSelection));
 
 function computerPlay(playArray){
 	
@@ -9,3 +16,25 @@ function computerPlay(playArray){
 
 }
 
+function playRound(playerSelection, computerSelection){
+	
+	playerSelection = playerSelection.toLowerCase();
+	computerSelection =  computerSelection.toLowerCase();
+	
+	if (computerSelection == playerSelection)
+		return("Tie Play!");	
+	
+	else if ((computerSelection =="rock" && playerSelection =="paper") || 
+			(computerSelection =="paper" && playerSelection =="scissors") ||
+			(computerSelection =="scissors" && playerSelection =="rock"))
+		return ("You Win! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection));
+	
+	else if ((computerSelection == "rock" && playerSelection =="scissors") || 
+			(computerSelection =="paper" && playerSelection =="rock") ||
+			(computerSelection =="scissors" && playerSelection =="paper"))
+		return ("You Lose! " + capitalize(playerSelection) + " loses against " + capitalize(computerSelection));
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
