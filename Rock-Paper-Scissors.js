cf333aa380b2382bd4974ce09444d4df623fa1fd
Ahmad Ben Jmii <ahmedblaugrana@gmail.com>
2022-06-07 16:34:38 +0200

    finish new design of game

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,4 +1,3 @@
-console.log("Hello World");
 const playArray = ["Rock", "Paper", "Scissors"];
 
 const buttonList = document.querySelectorAll('button');
@@ -6,7 +5,8 @@ const computerselection = document.getElementsByClassName("computer-selection")[
 const playerselection = document.getElementsByClassName("player-selection")[0];
 const result = document.getElementsByClassName("result")[0];
 
-//game();
+let computerScore = 0;
+let playerScore = 0;
 
 function computerPlay(playArray){
 	return playArray[Math.floor(Math.random() * playArray.length)];
@@ -23,12 +23,14 @@ function playRound(playerSelection, computerSelection){
 	else if ((computerSelection =="rock" && playerSelection =="paper") || 
 			(computerSelection =="paper" && playerSelection =="scissors") ||
 			(computerSelection =="scissors" && playerSelection =="rock")){
+		playerScore++ ;
 		return ("You Win! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection));
 			}
 	
 	else if ((computerSelection == "rock" && playerSelection =="scissors") || 
 			(computerSelection =="paper" && playerSelection =="rock") ||
 			(computerSelection =="scissors" && playerSelection =="paper")){
+		computerScore++ ;
 		return ("You Lose! " + capitalize(playerSelection) + " loses against " + capitalize(computerSelection));
 			}
 }
@@ -37,40 +39,10 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function game(playerScore,computerScore){
-	
 
-	var playerScore = 0; 
-	var computerScore = 0;
-	
-	let result = "";
-	let computerSelection,playerSelection;
-	
-	
-}
-
-let counter = 0;
-while (counter < 5){
-		buttonList.forEach(function(button){
-			button.addEventListener('click', clickHandler);
-		})
-/* 		computerSelection = computerPlay(playArray);
-		console.log(counter + "- Computer played : " + computerSelection);
-		
-		playerSelection = prompt("Enter your play: Paper, Rock or Scissors");
-		console.log(counter + "- You played : " + playerSelection);
-		
-		result = playRound(playerSelection,computerSelection);
-		console.log(result);
-		if(result.includes("Win"))
-			playerScore += 1;
-		else if (result.includes("Lose"))
-			computerScore += 1; */
-		alert("counter = " + counter);
-		counter += 1;
-	}
-	// console.log("End of game : PlayerScore " + playerScore + " - " + computerScore + " ComputerScore !");
-
+buttonList.forEach(function(button){
+	button.addEventListener('click', clickHandler);
+})
 
 function clickHandler(event) {
 	playerSelection = event.currentTarget.innerHTML;
@@ -78,5 +50,13 @@ function clickHandler(event) {
 	
 	computerselection.innerHTML = "Computer Selection ----------------- : " + computerSelection;
 	playerselection.innerHTML = "Player Selection ----------------- : " + playerSelection;
-	result.innerHTML = "Result ----------------- : " + playRound(playerSelection, computerSelection); 
+	result.innerHTML = "Result ----------------- : " + playRound(playerSelection, computerSelection);
+	result.innerHTML += "<br><br> Player -- " + playerScore + " || " + computerScore + " -- Computer";
+	if (playerScore === 5 || computerScore === 5){
+		if (playerScore > computerScore)
+			eog_msg ="Player wins the game";
+		else
+			eog_msg = "Computer wins the game";
+      alert("end of game : " + eog_msg);
+    }
 }
